2cec29f38af505806cb69e611fd481090ece19ba
paulincai <paulailincai@icloud.com>
2020-02-10 19:41:55 +0400

    FIxes #3

--- app/swSrc.js
+++ app/swSrc.js
@@ -1,126 +1,89 @@
 /* global importScripts, workbox, clients */
-/* eslint-disable no-restricted-globals */
-/* eslint-disable quotes */
-/* eslint-disable comma-dangle */
-/* eslint-disable quote-props */
 
-if (typeof window === 'function') {
-  importScripts('http://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js')
+const debug = true // When true the build is for development. Set false for Production version.
 
+/**
+ * Select the file strategy
+ * 1. When offline, serve the JS, CSS bundle and the present route if cached
+ * 2. When offline, server and HTML file
+ */
+const fileStrategy = 1
+
+if (typeof importScripts === 'function') {
+  importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js')
+  workbox.setConfig({ debug })
   const APP_SHELL = '/app-shell'
-  const HASHED_CACHE = 'hashedCache'
+  const BUNDLE_CACHE = 'bundleCache'
+  const { precaching /* , googleAnalytics */ } = workbox
+  const registerRoute = workbox.routing.registerRoute
+  const NavigationRoute = workbox.routing.NavigationRoute
+  const { NetworkFirst } = workbox.strategies
+  const { skipWaiting, clientsClaim } = workbox.core
 
-  // Precache all the files needed by the App Shell, as defined in workbox-config.js
-  // If any of these files are updated, run `npm run update-sw` to update this file automatically.
-  workbox.precaching.precacheAndRoute([])
+  /**
+   * Enable for offline google analytics
+   */
+  // googleAnalytics.initialize()
 
-  // Cache the App Shell route. We try to get latest from the network, fall back to cache for offline.
-  //
-  // Workbox's `registerNavigationRoute` helper will always try the cache first, and this causes Meteor's
-  // hot code pushing to enter an infinite loop (Meteor requests a reload, the service worker returns the old
-  // cached version, so Meteor requests a reload, and so on). So, rather than using the helper, we'll register
-  // a new `NavigationRoute` ourselves, so that we can supply a handler.
-  //
-  // We'd like to use the `NetworkFirst` strategy, but the normal version will always try to fetch and cache
-  // the route that's being accessed. We override the class to force the request to be our app-shell url.
+  precaching.precacheAndRoute([])
 
-  class NetworkFirstToFixedRoute extends workbox.strategies.NetworkFirst {
-    constructor (url, options = {}) {
-      super(options)
-      this.url = url
-    }
+  const strategy = new NetworkFirst(APP_SHELL)
+  registerRoute(new NavigationRoute(strategy))
 
-    async handle ({ event }) {
-      // console.log(event)
-      return this.handle({
-        event,
-        request: this.url,
-      })
-    }
-  }
+  skipWaiting() // when a worker is updated and sent to the client, do not wait for the user to manually confirm the swap
+  clientsClaim()
 
-  // Finally, we can register a navigation route with a the custom handler that always fetches from `APP_SHELL`;
-  workbox.routing.registerRoute(new workbox.routing.NavigationRoute(new NetworkFirstToFixedRoute(APP_SHELL)))
+  const match = /meteor_css_resource=true|meteor_js_resource=true/
 
-  workbox.clientsClaim()
-
-  // eslint-disable-next-line no-inner-declarations
-  function removeHash (element) {
-    if (typeof element === 'string') return element.split('?hash=')[0]
-    return element
-  }
-
-  // eslint-disable-next-line no-inner-declarations
-  function hasSameHash (firstUrl, secondUrl) {
-    if (typeof firstUrl === 'string' && typeof secondUrl === 'string') {
-      return /\?hash=(.*)/.exec(firstUrl)[1] === /\?hash=(.*)/.exec(secondUrl)[1]
-    }
-
-    return false
-  }
-
-  // Use our own cache for all hashed assets (Meteor generates the hashes for us)
-  // Old versions of a given URL are replaced when a version with a different hash is
-  // requested, otherwise we always return the cached version.
-  //
-  // We also need to make sure that we eventually delete files that are no longer used
-  // at all. We assume that once a file hasn't been used for a few weeks it's no longer
-  // useful. We use Workbox's CacheExpiration module to expire all the old files.
-
-  const expirationManager = new workbox.expiration.CacheExpiration(
-    HASHED_CACHE,
-    {
-      maxAgeSeconds: 4 * 7 * 24 * 60 * 60
-    }
-  )
-
-  workbox.routing.registerRoute(/\?hash=.*/, ({ url, event }) => {
-    // eslint-disable-next-line no-undef
-    caches.open(HASHED_CACHE).then((cache) => {
-      const req = event.request.clone()
-
-      return cache.match(req).then((cached) => {
-        // Return the cached version if the hash is the same (updating the timestamp in the expiration manager)
-        if (cached && hasSameHash(url.toString(), cached.url.toString())) {
-          return expirationManager.updateTimestamp(url.toString()).then(() => cached)
-        }
-
-        // Try to fetch it....
-        // eslint-disable-next-line no-undef
-        return fetch(req).then((response) => {
-          const clonedResponse = response.clone()
-
-          if (!clonedResponse || clonedResponse.status !== 200 || clonedResponse.type !== 'basic') {
-            return response
-          }
-
-          // Remove all other versions of this file frome the cache
-          const re = new RegExp(removeHash(url.toString()))
-          // eslint-disable-next-line no-undef
-          caches.open(HASHED_CACHE).then(hashCache => hashCache.keys().then(keys => keys.forEach((asset) => {
-            if (re.test(removeHash(asset.url.toString()))) {
-              hashCache.delete(asset)
+  const handlerCb = ({ url, event }) => {
+    return caches.open(BUNDLE_CACHE)
+      .then(bundleCache => {
+        return bundleCache
+          .match(url.pathname + url.search)
+          .then(cached => {
+            if (cached) {
+              // console.log('I am returning the cached: ', cached)
+              return cached
             }
-          })))
-
-          // Cache this version
-          // eslint-disable-next-line no-undef
-          caches.open(HASHED_CACHE)
-            .then(hashCache => hashCache.put(event.request, clonedResponse))
-            .then(() => expirationManager.updateTimestamp(url.toString()))
-
-          // Return it
-          return response
-        })
+            /**
+             * If not in cache, fetch and add to the cache
+             */
+            return fetch(event.request)
+              .then(response => {
+                const clonedResponse = response.clone()
+                const finalResponse = response.clone()
+                caches.open(BUNDLE_CACHE)
+                  .then(bundle => bundleCache.keys()
+                    .then(cacheNames => {
+                      cacheNames.forEach(cacheName => {
+                        const fileType = cacheName.url.substr(cacheName.url.indexOf('?'))
+                        if (fileType === url.search) {
+                          console.log('I delete a bundle delete this keys: ', cacheName)
+                          bundleCache.delete(cacheName)
+                        }
+                      })
+                    })
+                    .then(() => bundle.put(response.url, clonedResponse))
+                  )
+                  // .then(() => cacheExpiration.updateTimestamp(response.url))
+                  .catch(e => console.log(`Service worker threw here ${e}`))
+                return finalResponse
+              })
+              .catch(e => console.log(`Error fetching from live ${e}`))
+          })
+          .catch(e => console.log(`Error fetching from live ${e}`))
       })
-    })
-      .then(() => { expirationManager.expireEntries() })
-      .catch(e => console.log(`Service worker threw ${e}`))
-  })
+      .catch(e => console.log(`Service worker threw there ${e}`))
+  }
 
-  // Push event listener aux function:
+  if (fileStrategy === 1) {
+    registerRoute(match, handlerCb)
+  }
+
+  // THIS (below to the end) IS ONLY RELEVANT IF YOU USE PUSH NOTIFICATIONS FOR PWA
+  // Push event listener:
+
   const showNotification = evt => {
-    // eslint-disable-next-line no-undef
     if (!(self.Notification && self.Notification.permission === 'granted')) {
       return
     }
@@ -129,18 +92,17 @@ if (typeof window === 'function') {
     const options = {
       body: evt.data && evt.data.text() ? evt.data.text() : 'Push message no payload',
       tag: 'demo',
-      icon: 'https://your_notification_icon.png',
-      badge: 'https://your_notification_icon.png',
+      icon: 'https://assets.activitree.com/images/assets/icon-192x192.png', // these are sample images
+      badge: 'https://assets.activitree.com/images/assets/icon-192x192.png', // these are sample images
       // Custom actions buttons
       actions: [
         { action: 'yes', title: 'I ♥ this app!' },
-        { action: 'no', title: 'I don\'t like this app' },
-      ],
+        { action: 'no', title: 'I don\'t like this app' }
+      ]
     }
 
     evt.waitUntil(
-      // eslint-disable-next-line no-undef
-      self.registration.showNotification(title, options),
+      self.registration.showNotification(title, options)
     )
   }
 
@@ -156,7 +118,6 @@ if (typeof window === 'function') {
   // Source: https://developers.google.com/web/ilt/pwa/introduction-to-push-notifications
   // Source: https://developers.google.com/web/fundamentals/codelabs/push-notifications/
 
-  // eslint-disable-next-line no-undef
   self.addEventListener('push', evt => {
     console.log('[Service Worker] Push Received.')
     console.log(`[Service Worker] Push had this data: "${evt && evt.data}"`)
@@ -185,11 +146,9 @@ if (typeof window === 'function') {
   // Source: https://developers.google.com/web/fundamentals/codelabs/push-notifications/
   // Source: https://developers.google.com/web/ilt/pwa/introduction-to-push-notifications
 
-  // eslint-disable-next-line no-undef
   self.addEventListener('notificationclick', evt => {
     console.log('[Service Worker] Notification click Received.')
 
-    // eslint-disable-next-line no-undef
     const appUrl = new URL('/', location).href
 
     // Listen to custom action buttons in push notification
@@ -216,10 +175,8 @@ if (typeof window === 'function') {
         })
     )
 
-    // Close all notifications (thisincludes any other notifications from the
-    // same origin)
+    // Close all notifications (this includes any other notifications from the same origin)
     // Source: https://developers.google.com/web/ilt/pwa/introduction-to-push-notifications
-    // eslint-disable-next-line no-undef
     self.registration.getNotifications()
       .then((notifications) => {
         notifications.forEach((notification) => { notification.close() })
