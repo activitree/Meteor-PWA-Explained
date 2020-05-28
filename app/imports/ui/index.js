af38d5fdff48efa4d62fa935764a0519ab599ec1
Paul Ailincai <paulailincai@icloud.com>
2020-05-28 13:22:38 +0400

    Update index.js

--- app/imports/ui/index.js
+++ app/imports/ui/index.js
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor'
 
-// register a service worker when you don't use activitree:push
+// register a service worker when you don't use activitree:push for Web Push services in your Meteor web app
 
 async function main () {
 
